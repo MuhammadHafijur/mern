70a18d95b1502f4f8b9aec7ea077da1221e20609
MuhammadHafijur <hafijur.salah@gmail.com>
2022-09-27 10:53:34 +0600

    :sparkles: feat: login & signup forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import {
 
 // pages & components
 import Home from "./pages/Home";
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -18,6 +20,14 @@ function App() {
             <Route 
               path="/"
               element={<Home />}
+            />
+            <Route 
+              path="/login"
+              element={<Login />}
+            />
+            <Route 
+              path="/signup"
+              element={<Signup />}
             />
           </Routes>
         </div>
